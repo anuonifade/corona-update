ed169121829ed01dddd53a78135991e9dd01848e
andela-aonifade <anuoluwapo.onifade@andela.com>
2020-04-30 01:54:29 +0100

    changes map to use customly styled map

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -2,6 +2,6 @@ export const mapServices = [
   {
     name: 'OpenStreetMap',
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    url: 'https://api.mapbox.com/styles/v1/anuonifade/ck9ksxqgr1whc1inp1yr9wvkx/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW51b25pZmFkZSIsImEiOiJjazlseDU3cGkwOXlsM2xwYmkwOHdkYmtvIn0.uriwpn0qdOBDTZtWG0NC5Q'
   }
 ];
